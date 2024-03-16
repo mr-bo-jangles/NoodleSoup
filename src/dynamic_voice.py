515aa6511426b671f311ccdcc31e844dcf5a4000
WigglySuperNoodle <missturner333@gmail.com>
2024-03-16 19:56:26 +0000

    Moved towards database-based channel id and exclusion added db.json to gitignore

--- src/dynamic_voice.py
+++ src/dynamic_voice.py
@@ -4,6 +4,8 @@ import discord
 from discord import app_commands, VoiceState, Member
 from discord.ext import commands
 from discord.ext.commands import Cog
+from asynctinydb import Query, Document
+
 
 from src.utils import admin_check
 
@@ -28,9 +30,7 @@ class DynamicVoice(Cog):
         ) and self.in_chat_category(
             voice_state=after,
             channel_category_id=977917561243062322
-        ) and self.in_channel_name(
-                after,
-            "test"
+        ) and await self.is_in_channel_table(after.channel.id
         ):
             channel = discord.utils.get(member.guild.text_channels, name='dyno-logs')
             await channel.send("Curator just joined the test voice channel, creating and moving.")
@@ -40,6 +40,11 @@ class DynamicVoice(Cog):
             )
             # await new_channel.edit(status="I want to set a status too")
             await member.move_to(new_channel, reason="Moved member to generated voice channel")
+
+    async def is_in_channel_table(self, channel_id: int):
+        gen_channels = self.bot.db.table("gen_channels")
+        return await gen_channels.contains(doc_id=channel_id)
+
 
     def in_channel_name(self, voice_state, channel_name: str):
         return voice_state.channel.name == channel_name
@@ -58,7 +63,8 @@ class DynamicVoice(Cog):
                 user_limit=0,
                 video_quality_mode=discord.VideoQualityMode.auto
             )
-            channel_id = channel.id
+            gen_channels = self.bot.db.table("gen_channels")
+            await gen_channels.insert(Document({'name': channel.name}, doc_id=channel.id))
             await interaction.followup.send(f"{name} channel created in {category}.")
         else:
             await interaction.followup.send("This command is locked to Curators only.")
